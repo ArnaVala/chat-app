b96a5851d25c537d456307d417fbaa136c0e40d7
ArnaVala <arnavala@gmail.com>
2019-09-20 14:19:09 +0300

    add signIn/signUp components and routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './components/App';
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/signin' component={SignIn} />
+      <Route path='/signup' component={SignUp} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
