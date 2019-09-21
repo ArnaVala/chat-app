91404839078582c59427b467138bed835c648a7c
ArnaVala <arnavala@gmail.com>
2019-09-21 19:51:46 +0300

    add channel_reducer, put channel on global state

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
+import { setCurrentChannel } from '../../actions';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends React.Component {
@@ -10,6 +12,18 @@ class Channels extends React.Component {
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
     modal: false
+  };
+
+  componentDidMount() {
+    this.addListeners();
+  }
+
+  addListeners = () => {
+    let loadedChannels = [];
+    this.state.channelsRef.on('child_added', snap => {
+      loadedChannels.push(snap.val());
+      this.setState({ channels: loadedChannels });
+    });
   };
 
   addChannel = () => {
@@ -46,9 +60,27 @@ class Channels extends React.Component {
       this.addChannel();
     }
   };
+
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
+
+  changeChannel = channel => {
+    this.props.setCurrentChannel(channel);
+  };
+
+  displayChannels = channels =>
+    channels.length > 0 &&
+    channels.map(channel => (
+      <Menu.Item
+        key={channel.id}
+        onClick={() => this.changeChannel(channel)}
+        name={channel.name}
+        style={{ opacity: 0.7 }}
+      >
+        # {channel.name}
+      </Menu.Item>
+    ));
 
   isFormValid = ({ channelName, channelDetails }) =>
     channelName && channelDetails;
@@ -70,7 +102,7 @@ class Channels extends React.Component {
             </span>{' '}
             ({channels.length}) <Icon name='add' onClick={this.openModal} />
           </Menu.Item>
-          {/* Channels */}
+          {this.displayChannels(channels)}
         </Menu.Menu>
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Add a Channel</Modal.Header>
@@ -111,4 +143,7 @@ class Channels extends React.Component {
   }
 }
 
-export default Channels;
+export default connect(
+  null,
+  { setCurrentChannel }
+)(Channels);
