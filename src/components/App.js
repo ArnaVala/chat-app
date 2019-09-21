d2d97a0eb7577a14ee46e18153e4e854861e98d3
ArnaVala <arnavala@gmail.com>
2019-09-21 04:45:04 +0300

    scaffolding Basic Layout, App Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Grid, GridColumn } from 'semantic-ui-react';
+
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>App</div>;
-  }
-}
+const App = () => (
+  <Grid columns='equal' className='app' style={{ background: '#eee' }}>
+    <ColorPanel />
+    <SidePanel />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+    <GridColumn width={4}>
+      <MetaPanel />
+    </GridColumn>
+  </Grid>
+);
 
 export default App;
