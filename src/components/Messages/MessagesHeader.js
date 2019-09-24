9c0f7aa63c4459a060b680a70e69d9cb8b3cf229
ArnaVala <arnavala@gmail.com>
2019-09-24 10:49:35 +0300

    update starred props for user data upon starring channel

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Header, Segment, Input, Icon } from "semantic-ui-react";
+import React from 'react';
+import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 
 class MessagesHeader extends React.Component {
   render() {
@@ -8,29 +8,37 @@ class MessagesHeader extends React.Component {
       numUniqueUsers,
       handleSearchChange,
       searchLoading,
-      isPrivateChannel
+      isPrivateChannel,
+      handleStar,
+      isChannelStarred
     } = this.props;
 
     return (
       <Segment clearing>
         {/* Channel Title */}
-        <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
+        <Header fluid='true' as='h2' floated='left' style={{ marginBottom: 0 }}>
           <span>
-            {channelName}
-            {!isPrivateChannel && <Icon name={"star outline"} color="black" />}
+            {channelName}{' '}
+            {!isPrivateChannel && (
+              <Icon
+                onClick={handleStar}
+                name={isChannelStarred ? 'star' : 'star outline'}
+                color={isChannelStarred ? 'yellow' : 'black'}
+              />
+            )}
           </span>
           <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
 
         {/* Channel Search Input */}
-        <Header floated="right">
+        <Header floated='right'>
           <Input
             loading={searchLoading}
             onChange={handleSearchChange}
-            size="mini"
-            icon="search"
-            name="searchTerm"
-            placeholder="Search Messages"
+            size='mini'
+            icon='search'
+            name='searchTerm'
+            placeholder='Search Messages'
           />
         </Header>
       </Segment>
