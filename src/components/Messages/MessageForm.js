a0f1d4a393f9c9cbf1053dc94eab6f45e9b53a43
ArnaVala <arnavala@gmail.com>
2019-10-01 10:12:16 +0300

    add typing collection to track when other users are typing

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,18 +1,19 @@
-import React from "react";
-import uuidv4 from "uuid/v4";
-import firebase from "../../firebase";
-import { Segment, Button, Input } from "semantic-ui-react";
+import React from 'react';
+import uuidv4 from 'uuid/v4';
+import firebase from '../../firebase';
+import { Segment, Button, Input } from 'semantic-ui-react';
 
-import FileModal from "./FileModal";
-import ProgressBar from "./ProgressBar";
+import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
     storageRef: firebase.storage().ref(),
+    typingRef: firebase.database().ref('typing'),
     uploadTask: null,
-    uploadState: "",
+    uploadState: '',
     percentUploaded: 0,
-    message: "",
+    message: '',
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
@@ -28,6 +29,21 @@ class MessageForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleKeyDown = () => {
+    const { message, typingRef, channel, user } = this.state;
+    if (message) {
+      typingRef
+        .child(channel.id)
+        .child(user.uid)
+        .set(user.displayName);
+    } else {
+      typingRef
+        .child(channel.id)
+        .child(user.uid)
+        .remove();
+    }
+  };
+
   createMessage = (fileUrl = null) => {
     const message = {
       timestamp: firebase.database.ServerValue.TIMESTAMP,
@@ -38,16 +54,16 @@ class MessageForm extends React.Component {
       }
     };
     if (fileUrl !== null) {
-      message["image"] = fileUrl;
+      message['image'] = fileUrl;
     } else {
-      message["content"] = this.state.message;
+      message['content'] = this.state.message;
     }
     return message;
   };
 
   sendMessage = () => {
     const { getMessagesRef } = this.props;
-    const { message, channel } = this.state;
+    const { message, channel, user, typingRef } = this.state;
 
     if (message) {
       this.setState({ loading: true });
@@ -56,7 +72,11 @@ class MessageForm extends React.Component {
         .push()
         .set(this.createMessage())
         .then(() => {
-          this.setState({ loading: false, message: "", errors: [] });
+          this.setState({ loading: false, message: '', errors: [] });
+          typingRef
+            .child(channel.id)
+            .child(user.uid)
+            .remove();
         })
         .catch(err => {
           console.error(err);
@@ -67,7 +87,7 @@ class MessageForm extends React.Component {
         });
     } else {
       this.setState({
-        errors: this.state.errors.concat({ message: "Add a message" })
+        errors: this.state.errors.concat({ message: 'Add a message' })
       });
     }
   };
@@ -76,7 +96,7 @@ class MessageForm extends React.Component {
     if (this.props.isPrivateChannel) {
       return `chat/private-${this.state.channel.id}`;
     } else {
-      return "chat/public";
+      return 'chat/public';
     }
   };
 
@@ -87,12 +107,12 @@ class MessageForm extends React.Component {
 
     this.setState(
       {
-        uploadState: "uploading",
+        uploadState: 'uploading',
         uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
       },
       () => {
         this.state.uploadTask.on(
-          "state_changed",
+          'state_changed',
           snap => {
             const percentUploaded = Math.round(
               (snap.bytesTransferred / snap.totalBytes) * 100
@@ -103,7 +123,7 @@ class MessageForm extends React.Component {
             console.error(err);
             this.setState({
               errors: this.state.errors.concat(err),
-              uploadState: "error",
+              uploadState: 'error',
               uploadTask: null
             });
           },
@@ -117,7 +137,7 @@ class MessageForm extends React.Component {
                 console.error(err);
                 this.setState({
                   errors: this.state.errors.concat(err),
-                  uploadState: "error",
+                  uploadState: 'error',
                   uploadTask: null
                 });
               });
@@ -133,7 +153,7 @@ class MessageForm extends React.Component {
       .push()
       .set(this.createMessage(fileUrl))
       .then(() => {
-        this.setState({ uploadState: "done" });
+        this.setState({ uploadState: 'done' });
       })
       .catch(err => {
         console.error(err);
@@ -148,38 +168,39 @@ class MessageForm extends React.Component {
     const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
 
     return (
-      <Segment className="message__form">
+      <Segment className='message__form'>
         <Input
           fluid
-          name="message"
+          name='message'
+          onKeyDown={this.handleKeyDown}
           onChange={this.handleChange}
           value={message}
-          style={{ marginBottom: "0.7em" }}
-          label={<Button icon={"add"} />}
-          labelPosition="left"
+          style={{ marginBottom: '0.7em' }}
+          label={<Button icon={'add'} />}
+          labelPosition='left'
           className={
-            errors.some(error => error.message.includes("message"))
-              ? "error"
-              : ""
+            errors.some(error => error.message.includes('message'))
+              ? 'error'
+              : ''
           }
-          placeholder="Write your message"
+          placeholder='Write your message'
         />
-        <Button.Group icon widths="2">
+        <Button.Group icon widths='2'>
           <Button
             onClick={this.sendMessage}
             disabled={loading}
-            color="orange"
-            content="Add Reply"
-            labelPosition="left"
-            icon="edit"
+            color='orange'
+            content='Add Reply'
+            labelPosition='left'
+            icon='edit'
           />
           <Button
-            color="teal"
-            disabled={uploadState === "uploading"}
+            color='teal'
+            disabled={uploadState === 'uploading'}
             onClick={this.openModal}
-            content="Upload Media"
-            labelPosition="right"
-            icon="cloud upload"
+            content='Upload Media'
+            labelPosition='right'
+            icon='cloud upload'
           />
         </Button.Group>
         <FileModal
